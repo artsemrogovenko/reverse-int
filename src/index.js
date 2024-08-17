8cd63659c9dfdac6ddc37dfbd7f8b94a558f9b36
Artem_PC <taftoos2@gmail.com>
2024-08-17 10:08:37 +0300

    feat: int reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const reversedString = reverseNumber(n);
+
+    if (reversedString.toString().charAt(0) === '0') {
+        return parseInt(reversedString.slice(1));
+    } else {
+        return parseInt(reversedString);
+    }
+
+}
+
+function reverseNumber(number) {
+    return Math.abs(number).toString().split('').reverse().join('');
 }
